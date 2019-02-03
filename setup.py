21294ef13c7cc7a0d3baa36d26f2e715053d154a
Snooz <snooz@posteo.de>
2019-02-03 12:46:14 +0100

    updated setup.py and upload to PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-import setuptools
+from setuptools import setup
+from setuptools import find_packages
 import re
 from os.path import abspath, dirname, join
 
@@ -10,19 +11,21 @@ with open("README.md", "r") as fh:
 with open(join(CURDIR, 'src', 'DataDriver', 'DataDriver.py')) as f:
     VERSION = re.search("\n__version__ = '(.*)'", f.read()).group(1)
 
-setuptools.setup(
+setup(
     name="robotframework-datadriver",
     version=VERSION,
     author="René Rohner(Snooz82)",
     author_email="snooz@posteo.de",
-    description="A library for data driven tests.",
+    description="A library for Data-Driven Testing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Snooz82/robotframework-datadriver",
-    package_dir={'':'src'},
-    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Testing",
