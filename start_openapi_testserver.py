73593c6ff9bc44862bf94d0206f7f7b11eb9cb52
Robin Mackaij <r.mackaij@tkhsecurity.com>
2021-08-10 19:15:43 +0200

    Rework and expand test server

--- start_openapi_testserver.py
+++ start_openapi_testserver.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from typing import Dict, List, Optional
 from uuid import uuid4
 
@@ -9,69 +8,102 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class Species(str, Enum):
-    snake = "snake"
-    unicorn = "unicorn"
+class Message(BaseModel):
+    message: str
 
 
-class PetDetails(BaseModel):
+class WageGroup(BaseModel):
+    id: str
+    hourly_rate: float
+
+
+class EmployeeDetails(BaseModel):
     id: str
     name: str
-    price: Optional[float]
-    is_magical: bool
-    species: Species
+    employee_number: int
+    wage_group: WageGroup
 
 
-class Pet(BaseModel):
+class Employee(BaseModel):
     name: str
-    species: Species
-    price: Optional[float]
+    wage_group: WageGroup
 
 
-PETS: Dict[str, PetDetails] = {}
+WAGE_GROUPS: Dict[str, WageGroup] = {}
+EMPLOYEES: Dict[str, EmployeeDetails] = {}
+EMPLOYEE_NUMBERS = iter(range(1, 1000))
 
 
-@app.get("/", status_code=200)
-def get_root() -> Dict[str, str]:
-    return {"message": "Welcome to the pet store!"}
+@app.get("/", status_code=200, response_model=Message)
+def get_root() -> Message:
+    return Message(message="Welcome!")
 
 
-@app.post("/pets", status_code=201)
-def post_pet(pet: Pet) -> PetDetails:
-    if pet.species == Species.snake:
-        new_pet = PetDetails(
-            id=uuid4().hex,
-            is_magical=False,
-            **pet.dict(),
+@app.post("/wagegroups", status_code=201, response_model=WageGroup)
+def post_wagegroup(wagegroup: WageGroup) -> WageGroup:
+    if wagegroup.id in WAGE_GROUPS:
+        raise HTTPException(status_code=409, detail="Wage group already exists.")
+    WAGE_GROUPS[wagegroup.id] = wagegroup
+    return wagegroup
+
+
+@app.get("/wagegroups/{wagegroup_id}", status_code=200, response_model=WageGroup, responses={404: {"model": Message}})
+def get_wagegroup(wagegroup_id: str) -> WageGroup:
+    if wagegroup_id not in WAGE_GROUPS:
+        raise HTTPException(status_code=404, detail="Wage group not found")
+    return WAGE_GROUPS[wagegroup_id]
+
+
+@app.put("/wagegroups/{wagegroup_id}", status_code=200, response_model=WageGroup, responses={404: {"model": Message}})
+def put_wagegroup(wagegroup_id: str, wagegroup: WageGroup) -> WageGroup:
+    if wagegroup_id not in WAGE_GROUPS:
+        raise HTTPException(status_code=404, detail="Wage group not found.")
+    WAGE_GROUPS[wagegroup.id] = wagegroup
+    return wagegroup
+
+
+@app.delete("/wagegroups/{wagegroup_id}", status_code=204, response_model=None, responses={404: {"model": Message}, 403: {"model": Message}})
+def delete_wagegroup(wagegroup_id: str) -> None:
+    if wagegroup_id not in WAGE_GROUPS:
+        raise HTTPException(status_code=404, detail="Wage group not found.")
+    used_by = [e for e in EMPLOYEES.values() if e.wage_group.id == wagegroup_id]
+    if used_by:
+        raise HTTPException(
+            status_code=403, detail=f"Wage group still in use by {len(used_by)} employees."
         )
-    if pet.species == Species.unicorn:
-        new_pet = PetDetails(
-            id=uuid4().hex,
-            is_magical=True,
-            **pet.dict(),
-        )
-    PETS[new_pet.id] = new_pet
-    return new_pet
+    WAGE_GROUPS.pop(wagegroup_id)
 
 
-@app.put("/pets/{pet_id}", status_code=200)
-def put_pet(pet_id: str, pet: Pet) -> PetDetails:
-    if pet_id not in PETS:
-        raise HTTPException(status_code=404, detail="Pet not found")
-    updated_pet = PetDetails(
-        id=pet_id,
-        is_magical=PETS.get(pet_id).is_magical,
-        **pet.dict(),
+@app.post("/employees", status_code=201, response_model=EmployeeDetails)
+def post_employee(employee: Employee) -> EmployeeDetails:
+    new_employee = EmployeeDetails(
+        id=uuid4().hex,
+        employee_number=next(EMPLOYEE_NUMBERS),
+        **employee.dict(),
     )
-    PETS[pet_id] = updated_pet
-    return updated_pet
+    EMPLOYEES[new_employee.id] = new_employee
+    return new_employee
 
 
-@app.get("/pets/{pet_id}", status_code=200)
-def get_pet(pet_id: str) -> PetDetails:
-    if pet_id not in PETS:
-        raise HTTPException(status_code=404, detail="Pet not found")
-    return PETS.get(pet_id)
+@app.get("/employees/{employee_id}", status_code=200, response_model=EmployeeDetails, responses={404: {"model": Message}})
+def get_employee(employee_id: str) -> EmployeeDetails:
+    if employee_id not in EMPLOYEES:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    return EMPLOYEES[employee_id]
+
+
+@app.put("/employees/{employee_id}", status_code=200, response_model=EmployeeDetails, responses={404: {"model": Message}})
+def put_employee(employee_id: str, employee: Employee) -> EmployeeDetails:
+    if employee_id not in EMPLOYEES:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    current_employee_data = EMPLOYEES[employee_id]
+    updated_employee = EmployeeDetails(
+        id=employee_id,
+        employee_number=current_employee_data.employee_number,
+        **employee.dict(),
+    )
+    EMPLOYEES[employee_id] = updated_employee
+    return updated_employee
 
 
 def main():
