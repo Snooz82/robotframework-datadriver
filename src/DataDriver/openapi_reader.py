d4f308249ab30456cea0cbcab1f34a9250f91366
Robin Mackaij <r.mackaij@tkhsecurity.com>
2021-08-10 19:12:49 +0200

    Filter flag for FastAPI 422 on get / delete

--- src/DataDriver/openapi_reader.py
+++ src/DataDriver/openapi_reader.py
@@ -49,27 +49,35 @@ class OpenapiReader(AbstractReaderClass):
             for endpoint in ignored_endpoints:
                 endpoints.pop(endpoint, None)
         ignored_responses = self.reader_config.kwargs.get("ignored_responses", [])
+        ignore_fastapi_default_422 = self.reader_config.kwargs.get("ignore_fastapi_default_422", False)
         for endpoint, methods in endpoints.items():
             for method, method_data in methods.items():
                 for response in method_data.get("responses"):
+                    # FastAPI also adds a 422 response to endpoints that do not take
+                    # parameters that can be invalidated. Since header-invalidation is
+                    # currently not supported, these endpoints can be filtered by a
+                    # generic flag
+                    if response == "422" and method in ["get", "delete"] and ignore_fastapi_default_422:
+                        continue
                     # default applies to all status codes that are not specified, in
                     # which case we don't know what to expect and thus can't verify
-                    if response != "default" and response not in ignored_responses:
-                        tag_list: List[str] = []
-                        if tags:= method_data.get("tags", None):
-                            tag_list.extend(tags)
-                        tag_list.append(f"Method: {method.upper()}")
-                        tag_list.append(f"Response: {response}")
-                        test_data.append(
-                            TestCaseData(
-                                arguments={
-                                    "${endpoint}": endpoint,
-                                    "${method}": method.upper(),
-                                    "${status_code}": response,
-                                },
-                                tags=tag_list
-                            )
+                    if response == "default" or response  in ignored_responses:
+                        continue
+                    tag_list: List[str] = []
+                    if tags:= method_data.get("tags", None):
+                        tag_list.extend(tags)
+                    tag_list.append(f"Method: {method.upper()}")
+                    tag_list.append(f"Response: {response}")
+                    test_data.append(
+                        TestCaseData(
+                            arguments={
+                                "${endpoint}": endpoint,
+                                "${method}": method.upper(),
+                                "${status_code}": response,
+                            },
+                            tags=tag_list
                         )
+                    )
         return test_data
 
 
