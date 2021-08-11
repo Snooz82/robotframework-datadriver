3b1328753522a1a9411332f9f9154d3c23b9424b
Robin Mackaij <r.mackaij@tkhsecurity.com>
2021-08-11 19:34:29 +0200

    Expanding test server to cover more scenarios

--- start_openapi_testserver.py
+++ start_openapi_testserver.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Optional
 from uuid import uuid4
 
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -21,12 +21,18 @@ class EmployeeDetails(BaseModel):
     id: str
     name: str
     employee_number: int
-    wage_group: WageGroup
+    wagegroup_id: str
 
 
 class Employee(BaseModel):
     name: str
-    wage_group: WageGroup
+    wagegroup_id: str
+
+
+class EmployeeUpdate(BaseModel):
+    name: Optional[str]
+    employee_number: Optional[int]
+    wagegroup_id: Optional[str]
 
 
 WAGE_GROUPS: Dict[str, WageGroup] = {}
@@ -39,7 +45,12 @@ def get_root() -> Message:
     return Message(message="Welcome!")
 
 
-@app.post("/wagegroups", status_code=201, response_model=WageGroup)
+@app.post(
+    "/wagegroups",
+    status_code=201,
+    response_model=WageGroup,
+    responses={409: {"model": Message}},
+)
 def post_wagegroup(wagegroup: WageGroup) -> WageGroup:
     if wagegroup.id in WAGE_GROUPS:
         raise HTTPException(status_code=409, detail="Wage group already exists.")
@@ -47,14 +58,24 @@ def post_wagegroup(wagegroup: WageGroup) -> WageGroup:
     return wagegroup
 
 
-@app.get("/wagegroups/{wagegroup_id}", status_code=200, response_model=WageGroup, responses={404: {"model": Message}})
+@app.get(
+    "/wagegroups/{wagegroup_id}",
+    status_code=200,
+    response_model=WageGroup,
+    responses={404: {"model": Message}},
+)
 def get_wagegroup(wagegroup_id: str) -> WageGroup:
     if wagegroup_id not in WAGE_GROUPS:
         raise HTTPException(status_code=404, detail="Wage group not found")
     return WAGE_GROUPS[wagegroup_id]
 
 
-@app.put("/wagegroups/{wagegroup_id}", status_code=200, response_model=WageGroup, responses={404: {"model": Message}})
+@app.put(
+    "/wagegroups/{wagegroup_id}",
+    status_code=200,
+    response_model=WageGroup,
+    responses={404: {"model": Message}},
+)
 def put_wagegroup(wagegroup_id: str, wagegroup: WageGroup) -> WageGroup:
     if wagegroup_id not in WAGE_GROUPS:
         raise HTTPException(status_code=404, detail="Wage group not found.")
@@ -62,20 +83,37 @@ def put_wagegroup(wagegroup_id: str, wagegroup: WageGroup) -> WageGroup:
     return wagegroup
 
 
-@app.delete("/wagegroups/{wagegroup_id}", status_code=204, response_model=None, responses={404: {"model": Message}, 403: {"model": Message}})
+@app.delete(
+    "/wagegroups/{wagegroup_id}",
+    status_code=204,
+    response_class=Response,
+    responses={404: {"model": Message}, 403: {"model": Message}},
+)
 def delete_wagegroup(wagegroup_id: str) -> None:
     if wagegroup_id not in WAGE_GROUPS:
         raise HTTPException(status_code=404, detail="Wage group not found.")
-    used_by = [e for e in EMPLOYEES.values() if e.wage_group.id == wagegroup_id]
+    used_by = [e for e in EMPLOYEES.values() if e.wagegroup_id == wagegroup_id]
     if used_by:
         raise HTTPException(
-            status_code=403, detail=f"Wage group still in use by {len(used_by)} employees."
+            status_code=403,
+            detail=f"Wage group still in use by {len(used_by)} employees."
         )
     WAGE_GROUPS.pop(wagegroup_id)
 
 
-@app.post("/employees", status_code=201, response_model=EmployeeDetails)
+@app.post(
+    "/employees",
+    status_code=201,
+    response_model=EmployeeDetails,
+    responses={400: {"model": Message}},
+)
 def post_employee(employee: Employee) -> EmployeeDetails:
+    wagegroup_id = employee.wagegroup_id
+    if wagegroup_id not in WAGE_GROUPS:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Wage group with id {wagegroup_id} does not exist."
+        )
     new_employee = EmployeeDetails(
         id=uuid4().hex,
         employee_number=next(EMPLOYEE_NUMBERS),
@@ -85,23 +123,30 @@ def post_employee(employee: Employee) -> EmployeeDetails:
     return new_employee
 
 
-@app.get("/employees/{employee_id}", status_code=200, response_model=EmployeeDetails, responses={404: {"model": Message}})
+@app.get(
+    "/employees/{employee_id}",
+    status_code=200,
+    response_model=EmployeeDetails,
+    responses={404: {"model": Message}},
+)
 def get_employee(employee_id: str) -> EmployeeDetails:
     if employee_id not in EMPLOYEES:
         raise HTTPException(status_code=404, detail="Employee not found")
     return EMPLOYEES[employee_id]
 
 
-@app.put("/employees/{employee_id}", status_code=200, response_model=EmployeeDetails, responses={404: {"model": Message}})
-def put_employee(employee_id: str, employee: Employee) -> EmployeeDetails:
+@app.patch(
+    "/employees/{employee_id}",
+    status_code=200,
+    response_model=EmployeeDetails,
+    responses={404: {"model": Message}},
+)
+def patch_employee(employee_id: str, employee: EmployeeUpdate) -> EmployeeDetails:
     if employee_id not in EMPLOYEES:
         raise HTTPException(status_code=404, detail="Employee not found")
-    current_employee_data = EMPLOYEES[employee_id]
-    updated_employee = EmployeeDetails(
-        id=employee_id,
-        employee_number=current_employee_data.employee_number,
-        **employee.dict(),
-    )
+    stored_employee_data = EMPLOYEES[employee_id]
+    employee_update_data = employee.dict(exclude_unset=True)
+    updated_employee = stored_employee_data.copy(update=employee_update_data)
     EMPLOYEES[employee_id] = updated_employee
     return updated_employee
 
